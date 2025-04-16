07888ff2c589a5e4f968ab548bb74d7cb2322316
PINKeHamton <PinkHamton@protonmail.com>
2025-04-15 20:49:52 -0500

    maps

---
+++ src/map.c
@@ -0,0 +1,12 @@
+#define MAPSIZE 511 // 0-511 so just 512 tiles
+#define TILESETSIZE 16
+//               X        Y
+char mapTable[MAPSIZE][MAPSIZE];
+char name[5]; // EXMP: W#M# is the naming standerd and it MAXS at W##M##
+
+typedef enum {TILES = 0, ENTITES} TileType;
+
+void loadMap() {
+  
+}
+
